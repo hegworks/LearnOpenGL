c09c6bcb4c56342937e9cd3a455641926d4c36a3
Hesam Ghadimi <hess.gha@gmail.com>
2024-11-09 18:47:17 +0100

    add glCheckError()

--- src/game.cpp
+++ src/game.cpp
@@ -7,6 +7,28 @@
 #include <windows.h>
 #endif
 
+GLenum glCheckError_(const char* file, int line)
+{
+	GLenum errorCode;
+	while((errorCode = glGetError()) != GL_NO_ERROR)
+	{
+		std::string error;
+		switch(errorCode)
+		{
+			case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
+			case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
+			case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
+			case 1283:							   error = "STACK_OVERFLOW"; break;
+			case 1284:							   error = "STACK_UNDERFLOW"; break;
+			case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
+			case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+		}
+		std::cout << "GL ERROR: " << errorCode << " | " << error << " | " << file << " (" << line << ")" << std::endl;
+	}
+	return errorCode;
+}
+#define glCheckError() glCheckError_(__FILE__, __LINE__) 
+
 const char* vertexShaderSource =
 "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
