7294e1a718c3fa01e2fe2a0f06ce25ca22411bb7
Hesam Ghadimi <hess.gha@gmail.com>
2024-11-09 15:32:35 +0100

    EBO (rectangle) + reformat

--- src/game.cpp
+++ src/game.cpp
@@ -36,72 +36,9 @@ void processInput(GLFWwindow* window)
 		glfwSetWindowShouldClose(window, true);
 }
 
-void Render()
+unsigned int InitializeShaderProgram()
 {
-
-}
-
-int main(int argc, char* argv[])
-{
-	printf("Hello world\n");
-
-#ifdef _WIN32
-	CONSOLE_SCREEN_BUFFER_INFO coninfo;
-	AllocConsole();
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
-	coninfo.dwSize.X = 700;
-	coninfo.dwSize.Y = 9999;
-	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coninfo.dwSize);
-	SetWindowPos(GetConsoleWindow(), HWND_TOP, 0, 0, 700, 1000, 0);
-#endif
-
-	glfwInit();
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_POSITION_X, 700);
-	glfwWindowHint(GLFW_POSITION_Y, 80);
-
-	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-	if(window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-
-	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-	{
-		std::cout << "Failed to initialize GLAD" << std::endl;
-		return -1;
-	}
-
-	glViewport(0, 0, 800, 600);
-
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-	printf("OpenGL version: %s\n", glGetString(GL_VERSION));
-
-
-
-	//------------------------------------------------
-	// VertexBufferObject
-	// -----
-	float vertices[] =
-	{
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f,  0.5f, 0.0f
-	};
-
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// VertexShader
-	// -----
+	//-----VertexShader
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -116,9 +53,9 @@ int main(int argc, char* argv[])
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
+	//=====VertexShader
 
-	// FragmentShader
-	// -----
+	//-----FragmentShader
 	unsigned int fragmentShader;
 	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
@@ -133,9 +70,9 @@ int main(int argc, char* argv[])
 		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
+	//=====FragmentShader
 
-	// ShaderProgram
-	// -----
+	//-----ShaderProgram
 	unsigned int shaderProgram;
 	shaderProgram = glCreateProgram();
 	glAttachShader(shaderProgram, vertexShader);
@@ -153,20 +90,109 @@ int main(int argc, char* argv[])
 		glGetShaderInfoLog(shaderProgram, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::PROGRAM::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
+	//=====ShaderProgram
 
-	// VertexArrayObject
-	// -----
+	return shaderProgram;
+}
+
+GLFWwindow* WindowSetup()
+{
+#ifdef _WIN32
+	CONSOLE_SCREEN_BUFFER_INFO coninfo;
+	AllocConsole();
+	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
+	coninfo.dwSize.X = 700;
+	coninfo.dwSize.Y = 9999;
+	SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coninfo.dwSize);
+	SetWindowPos(GetConsoleWindow(), HWND_TOP, 0, 0, 700, 1000, 0);
+#endif
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_POSITION_X, 700);
+	glfwWindowHint(GLFW_POSITION_Y, 100);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if(window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		throw std::exception("Failed to create GLFW window");
+	}
+	glfwMakeContextCurrent(window);
+
+	if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		throw std::exception("Failed to initialize GLAD");
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	printf("OpenGL version: %s\n", glGetString(GL_VERSION));
+
+	return window;
+}
+
+int main(int argc, char* argv[])
+{
+	printf("Hello world\n");
+
+	GLFWwindow* window = WindowSetup();
+
+	unsigned int shaderProgram = InitializeShaderProgram();
+
+	//----------objects initialization
+	//-----points
+	float vertices[] =
+	{
+	 0.5f,  0.5f, 0.0f,  // 0 top right
+	 0.5f, -0.5f, 0.0f,  // 1 bottom right
+	-0.5f, -0.5f, 0.0f,  // 2 bottom left
+	-0.5f,  0.5f, 0.0f   // 3 top left 
+	};
+	unsigned int indices[] =
+	{
+		0, 1, 3,   // first triangle
+		1, 2, 3    // second triangle
+	};
+	//=====points
+
+	//-----Initialization
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	//=====Initialization
+
+	//-----Binding
 	glBindVertexArray(VAO);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	//=====Binding
+
+	// VertexAttribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(0));
 	glEnableVertexAttribArray(0);
-	//------------------------------------------------
 
+	// UnBinding
+	glBindVertexArray(0);
+	//==========objects initialization
 
-
-
+	// Wireframe
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // comment out for default behavior
 
 	while(!glfwWindowShouldClose(window))
 	{
@@ -177,23 +203,14 @@ int main(int argc, char* argv[])
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		//-----render
 
-
-
-
-
-		//------------------------------------------------
-		//Render();
-
-		// use program
 		glUseProgram(shaderProgram);
-
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-		//------------------------------------------------
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
-
-
+		//=====render
 
 		// check and call events and swap the buffers
 		glfwSwapBuffers(window);
