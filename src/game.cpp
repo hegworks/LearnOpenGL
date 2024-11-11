8cc9255181efa905cc15bfd3cdb00b6bddc005a5
Hesam Ghadimi <hess.gha@gmail.com>
2024-11-11 16:38:26 +0100

    add glm

--- src/game.cpp
+++ src/game.cpp
@@ -2,6 +2,9 @@
 #include <cstdio>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
 #include "Shader.h"
@@ -110,6 +113,12 @@ GLFWwindow* WindowSetup()
 int main(int argc, char* argv[])
 {
 	printf("Hello world\n");
+
+	glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+	glm::mat4 trans = glm::mat4(1.0f);
+	trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+	vec = trans * vec;
+	std::cout << vec.x << vec.y << vec.z << std::endl;
 
 	GLFWwindow* window = WindowSetup();
 	if(window == nullptr) return -1;
