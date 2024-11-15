2672a1e75e322141e575a858aaa748d9363a988a
Hesam Ghadimi <hess.gha@gmail.com>
2024-11-15 22:47:12 +0100

    reformat: move glCheckError to separate file

--- src/game.cpp
+++ src/game.cpp
@@ -15,30 +15,9 @@
 #ifdef _WIN32
 #include <windows.h>
 #endif
+#include "Tools/GlCheckError.h"
 
 Camera* m_freeFlyCamera = nullptr;
-
-GLenum glCheckError_(const char* file, int line)
-{
-	GLenum errorCode;
-	while((errorCode = glGetError()) != GL_NO_ERROR)
-	{
-		std::string error;
-		switch(errorCode)
-		{
-			case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
-			case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
-			case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
-			case 1283:							   error = "STACK_OVERFLOW"; break;
-			case 1284:							   error = "STACK_UNDERFLOW"; break;
-			case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
-			case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
-		}
-		std::cout << "GL ERROR: " << errorCode << " | " << error << " | " << file << " (" << line << ")" << std::endl;
-	}
-	return errorCode;
-}
-#define glCheckError() glCheckError_(__FILE__, __LINE__) 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
